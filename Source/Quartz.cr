419532c6b159720bf9114670ab1eea0df587c186
Yutsuna <leo.quinzler@epitech.eu>
2026-06-13 22:19:19 +0200

    Chore(Quartz): Add template for REPL

--- Source/Quartz.cr
+++ Source/Quartz.cr
@@ -1,1 +1,18 @@
 require "./Quartz/**"
+
+# class User < Quartz::AModel
+#   field name : String
+#   field age : Int32
+# end
+
+# User.objects.create( name: "Léo", age: 24 )
+# User.objects.create( name: "Jacques", age: 53 )
+# User.objects.create( name: "Xavier", age: 16 )
+
+# adults = User.objects
+#   .filter { |u| u.age >= 18 }
+#   .exclude { |u| u.name == "admin" }
+#   .order_by { |u| u.age  }
+#   .limit ( 10 )
+
+# adults.to_a
